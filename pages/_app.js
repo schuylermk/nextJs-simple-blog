f471e96c1092d552bfb46f5dade4f4dcf6092bec
schuylermk <schuyler.m.k@gmail.com>
2023-07-13 13:11:46 -0700

    Sets up prismjs to allow for better-looking code snippets

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css';
+import 'prismjs/themes/prism.css';
+import 'prismjs';
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
